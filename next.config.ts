7d374a0b37e65ffa6b454a7b8a3df38f24a39e65
Jakub Coufal <coufalja@gmail.com>
2026-01-13 07:46:46 +0100

    Fix linter found issues

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
+    qualities: [75, 90, 100],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
